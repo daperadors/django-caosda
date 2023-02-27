55b744344f6c5880fbc2fcb8abdabeed0109d34d
ies-sabadell <ies-sabadell>
2023-02-27 19:14:54 +0100

    finished v2

--- django_caosda/urls.py
+++ django_caosda/urls.py
@@ -17,12 +17,13 @@ from django.contrib import admin
 from django.urls import path
 
 from videojocs.views import home, videogames, platforms, platformRandom, platformEdit, platformDelete, platformRandomNewGames, usersPlatform, remove_videojoc, add_videojoc
-from users.views import login
+from users.views import LoginView, LogoutView
 from users.views import register
 
 urlpatterns = [
     path('admin/', admin.site.urls, name="admin"),
-    path('login/', login.as_view(), name="login"),
+    path('login/', LoginView.as_view(), name="login"),
+    path('logout/', LogoutView.as_view(), name="logout"),
     path('register/', register.as_view(), name="register"),
     path('', home.as_view(), name="home"),
     path('videogames/', videogames.as_view(), name="videogames"),
@@ -33,8 +34,9 @@ urlpatterns = [
     path('randomPlatform/', platformRandom.as_view(), name="random_platform"),
     path('randomPlatformNewGames/', platformRandomNewGames.as_view(), name="random_platform_newgames"),
     path('platforms/delete/<int:id>', platformDelete.as_view(), name="delete_platform"),
-    path('platforms/add/<int:id1>/<int:id2>', usersPlatform.as_view(), name="associate_user_to_platform"),
+    path('platforms/add/<int:id1>/<int:id2>', usersPlatform.as_view(), name="associate_user_into_platform"),
     path('plataformesde/<str:name>', usersPlatform.as_view(), name="user_platform_search"),
     path('removeVideojoc/<int:videogame_id>/<int:user_id>', remove_videojoc.as_view()),
-    path('videojoc/<int:videogame_id>/<int:user_id>', add_videojoc.as_view())
+    path('videojoc/<int:videogame_id>/<int:user_id>', add_videojoc.as_view()),
+    path('plataformade/<str:name>', platforms.as_view())
 ]
